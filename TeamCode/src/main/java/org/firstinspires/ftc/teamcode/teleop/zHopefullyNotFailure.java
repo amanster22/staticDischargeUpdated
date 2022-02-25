85ccf899dd6a88fa35ef4b99454d9a1a95779d15
BobCheese314 <bobcheese.314@gmail.com>
2022-02-25 11:55:51 -0500

    added intake possibilities

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/zHopefullyNotFailure.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/zHopefullyNotFailure.java
@@ -11,28 +11,62 @@ public class zHopefullyNotFailure extends OpMode {
 
     public DcMotor capping = null;
     public Servo cappingServo = null;
-
+    public DcMotor inLeft = null;
+    public DcMotor inRight = null;
+    double potato = 0;
 
     @Override
     public void init() {
 
         capping = hardwareMap.dcMotor.get("cap");
         cappingServo = hardwareMap.servo.get("capServo");
+        inLeft = hardwareMap.dcMotor.get("left");
+        inRight = hardwareMap.dcMotor.get("right");
 
-    }
+}
 
     @Override
     public void loop() {
-        double stick = gamepad2.left_stick_y;
+
         capping.setPower(.4 * gamepad2.right_stick_y);
 
-        telemetry.addData("Value", stick);
+        telemetry.addData("Value=", potato);
         telemetry.update();
-        if (gamepad2.dpad_up) {
+
+        if (gamepad2.right_bumper) {
             cappingServo.setPosition(1);
+            potato = 1;
+        } else if (gamepad2.left_bumper) {
+            cappingServo.setPosition(.8);
+            potato = -1;
+        } //capping claw
+
+        if (gamepad2.a) {
+            inLeft.setPower(.5);
+            inRight.setPower(-.5);
         }
-        else if (gamepad2.dpad_down) {
-            cappingServo.setPosition(0.8);
+
+        if (gamepad2.b) {
+            inLeft.setPower(-.5);
+            inRight.setPower(.5);
         }
-    }//the loop ends here
-}
+
+        if (gamepad2.y) {
+            inLeft.setPower(-.5);
+            inRight.setPower(-.5);
+        }
+
+        if (gamepad2.x) {
+            inLeft.setPower(.5);
+            inRight.setPower(.5);
+        }
+        //all possibilities of intake
+
+        if (gamepad2.dpad_down) {
+            inRight.setPower(0);
+            inLeft.setPower(0);
+        }
+
+        }//the loop ends here
+    }
+
