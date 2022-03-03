1172321e7135ddeea1d0fd1722359dd3d8b9194f
BobCheese314 <bobcheese.314@gmail.com>
2022-03-03 14:07:22 -0500

    added intake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/zHopefullyNotFailure.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/zHopefullyNotFailure.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -9,64 +10,67 @@ import com.qualcomm.robotcore.hardware.Servo;
 @TeleOp(name = "Avneesh Testing", group = "Linear OpMode")
 public class zHopefullyNotFailure extends OpMode {
 
-    public DcMotor capping = null;
-    public Servo cappingServo = null;
-    public DcMotor inLeft = null;
-    public DcMotor inRight = null;
+//    public DcMotor capping = null;
+//    public Servo cappingServo = null;
+    public DcMotor intakeStars = null;
+    public DcMotor armWheels = null;
+    public DcMotor intakeTop = null;
+    public Servo upperRoller = null;
     double potato = 0;
 
     @Override
     public void init() {
 
-        capping = hardwareMap.dcMotor.get("cap");
-        cappingServo = hardwareMap.servo.get("capServo");
-        inLeft = hardwareMap.dcMotor.get("left");
-        inRight = hardwareMap.dcMotor.get("right");
+//        capping = hardwareMap.dcMotor.get("cap");
+//        cappingServo = hardwareMap.servo.get("capServo");
+        armWheels = hardwareMap.dcMotor.get("armWheels");
+        intakeStars = hardwareMap.dcMotor.get("stars");
+        intakeTop = hardwareMap.dcMotor.get("inTop");
+        upperRoller = hardwareMap.servo.get("upper");
+
 
 }
 
     @Override
     public void loop() {
 
-        capping.setPower(.4 * gamepad2.right_stick_y);
+//        capping.setPower(.4 * gamepad2.right_stick_y);
 
         telemetry.addData("Value=", potato);
         telemetry.update();
 
-        if (gamepad2.right_bumper) {
-            cappingServo.setPosition(1);
-            potato = 1;
-        } else if (gamepad2.left_bumper) {
-            cappingServo.setPosition(.8);
-            potato = -1;
-        } //capping claw
+//        if (gamepad2.right_bumper) {
+//            cappingServo.setPosition(1);
+//            potato = 1;
+//        } else if (gamepad2.left_bumper) {
+//            cappingServo.setPosition(.8);
+//            potato = -1;
+//        } //capping claw
 
-        if (gamepad2.a) {
-            inLeft.setPower(.5);
-            inRight.setPower(-.5);
+        if (gamepad1.a) {//intake on
+            intakeStars.setPower(0.5);
+            armWheels.setPower(-.75);
+            intakeTop.setPower(-.75);
+            upperRoller.setPosition(0);
+            //lights.setPattern(RevBlinkinLedDriver.BlinkinPattern.GREEN);
+            telemetry.addData("Intake:", true);
+
+
         }
-
-        if (gamepad2.b) {
-            inLeft.setPower(-.5);
-            inRight.setPower(.5);
+        if(gamepad1.b){//intake reverse
+            intakeStars.setPower(-0.5);
+            armWheels.setPower(1);
+            //rollerServo.setPosition(0);
+            upperRoller.setPosition(1);
+            telemetry.addData("Intake:", false);
+         //   lights.setPattern(RevBlinkinLedDriver.BlinkinPattern.HOT_PINK);
         }
-
-        if (gamepad2.y) {
-            inLeft.setPower(-.5);
-            inRight.setPower(-.5);
+        if(gamepad1.x){//intake off
+            intakeStars.setPower(0);
+            armWheels.setPower(0);
+            upperRoller.setPosition(-0.5);
+            telemetry.addData("Intake:", false);
         }
-
-        if (gamepad2.x) {
-            inLeft.setPower(.5);
-            inRight.setPower(.5);
-        }
-        //all possibilities of intake
-
-        if (gamepad2.dpad_down) {
-            inRight.setPower(0);
-            inLeft.setPower(0);
-        }
-
         }//the loop ends here
     }
 
